bee4ac72681ff5f917f5e92f9d85099bb45f9966
HagarAbouroumia <48414362+HagarAbouroumia@users.noreply.github.com>
2022-09-28 16:50:01 +0200

    Update e15.cpp
    * Overriding a method in the subclass
    * the effects of the overriding may be reversed (
    * uppercasting

--- e15.cpp
+++ e15.cpp
@@ -10,12 +10,12 @@ public:
 class Cat : public Pet {
 public:
 	Cat(string n) : Pet(n) { }
-	void MakeSound(void) { cout << Name << " the Cat says: Meow! Meow!" << endl; }
+	void MakeSound(void) { cout << Name << " the Cat says: Meow! Meow!" << endl; } //overriding 
 };
 class Dog : public Pet {
 public:
 	Dog(string n) : Pet(n) { }
-	void MakeSound(void) { cout << Name << " the Dog says: Woof! Woof!" << endl; }
+	void MakeSound(void) { cout << Name << " the Dog says: Woof! Woof!" << endl; } //overriding 
 };
 int main(void) {
 	Cat *a_cat;
@@ -24,8 +24,8 @@ int main(void) {
 	a_cat = new Cat("Kitty");
 	a_dog = new Dog("Doggie");
 	a_cat -> MakeSound();
-	static_cast<Pet *>(a_cat) -> MakeSound();
+	static_cast<Pet *>(a_cat) -> MakeSound(); // uppercasting
 	a_dog -> MakeSound();
-	static_cast<Pet *>(a_dog) -> MakeSound();
+	static_cast<Pet *>(a_dog) -> MakeSound(); // uppercasting
 	return 0;
 }
