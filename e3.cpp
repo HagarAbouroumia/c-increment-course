c418a828cc4ef5d10302e0b68ca82daeeca4cb93
HagarAbouroumia <48414362+HagarAbouroumia@users.noreply.github.com>
2022-09-28 15:25:48 +0200

    Create e3.cpp
    we added the public access specifier

---
+++ e3.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+class super{
+    public:
+    int x;
+};
+
+class sub : public super {
+   public:
+   int y;
+   
+};
+
+int main() {
+    sub s;
+    s.x = 5;
+    return 0;
+}
