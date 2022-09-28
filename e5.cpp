17cd3c40695f7f37d5b10f99888862579c6d06fc
HagarAbouroumia <48414362+HagarAbouroumia@users.noreply.github.com>
2022-09-28 15:36:44 +0200

    protected access specifier
    now the child class can access the p variable from the parent class.

---
+++ e5.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+class super{
+    protected:
+    int p = 5;
+};
+
+class sub : public super {
+    public:
+    void print_p(){
+        cout << p << endl;
+    }
+};
+
+int main() {
+    sub s;
+    s.print_p();
+}
